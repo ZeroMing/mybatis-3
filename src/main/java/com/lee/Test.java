44670f77f612ca33f02ab593697d090053749caa
ming_lee <zeroming@163.com>
2019-05-10 20:57:27 +0800

    测试文件

---
+++ src/main/java/com/lee/Test.java
@@ -0,0 +1,54 @@
+package com.lee;
+
+import com.lee.dao.UserDao;
+import com.lee.entity.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * @description: TODO-Eden.Lee
+ * @author: Eden.Lee/李明
+ * @company: 朴新教育
+ * @date: 2019/5/8 19:23
+ */
+public class Test {
+
+
+        public static void main(String[] args) throws IOException {
+            String resouce = "mybatis-conf.xml";
+            InputStream is = Resources.getResourceAsStream(resouce);
+
+            // 构建sqlSession工厂
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+            // 获取sqlSession
+            SqlSession session = sqlSessionFactory.openSession();
+
+            User user;
+
+            /*try {
+                *//**
+                 * 第一种方式: 直接执行已映射的 SQL 语句
+                 *//*
+                String statement = "com.luoxn28.dao.UserDao.getById";
+                user = session.selectOne(statement, 1);
+                System.out.println(user);
+            }
+            finally {
+                session.close();
+            }*/
+
+            /**
+             * 第二种方式: 执行更清晰和类型安全的代码
+             */
+            UserDao userDao = session.getMapper(UserDao.class);
+            user = userDao.getById(1);
+            System.out.println(user);
+        }
+
+
+}
---
+++ src/main/java/com/lee/dao/UserDao.java
@@ -0,0 +1,15 @@
+package com.lee.dao;
+
+import com.lee.entity.User;
+
+/**
+ * @description: TODO-Eden.Lee
+ * @author: Eden.Lee/李明
+ * @company: 朴新教育
+ * @date: 2019/5/8 16:22
+ */
+public interface UserDao {
+
+    User getById(Integer id);
+
+}
